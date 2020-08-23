a5df9392f24401c432727fff2c5fc60e54ffaf9a
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-08-23 17:15:40 -0400

    made changes to erb files

--- record_holder/db/schema.rb
+++ record_holder/db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_08_23_162604) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
+    t.string "username"
   end
 
 end
