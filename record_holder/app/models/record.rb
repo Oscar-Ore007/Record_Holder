891c3ac5737525b9bcec01e35f72f41ea7547df8
Oscar Ore <60165972+Oscar-Ore007@users.noreply.github.com>
2020-08-23 19:55:37 -0400

    added error messaging adding an erb folder

--- record_holder/app/models/record.rb
+++ record_holder/app/models/record.rb
@@ -1,3 +1,4 @@
 class Record < ActiveRecord::Base 
     belongs_to :user 
+    validates :genre, :artist_name, :album_name, presence: true 
 end 
--- record_holder/app/models/user.rb
+++ record_holder/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base 
     has_many :records 
-
     has_secure_password 
+    validates_uniqueness_of :email, :username, :password_digest, presence: true 
 end 
